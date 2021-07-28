d7a7f3ac98228de0e6a346fda2bfa7223577c0fa
Ashritha <ashrithapb@gmail.com>
2021-07-28 01:42:52 -0500

    first commit

---
+++ src/test/java/SeleniumAutomation/MavenFramework/SeleniumTest.java
@@ -0,0 +1,15 @@
+package SeleniumAutomation.MavenFramework;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void test1() {
+		System.out.println("Demo1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("Demo2");
+	}
+}
---
+++ src/test/java/SeleniumAutomation/MavenFramework/SeleniumTest2.java
@@ -0,0 +1,15 @@
+package SeleniumAutomation.MavenFramework;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest2 {
+
+	@Test
+	public void test3() {
+		System.out.println("Demo3");
+	}
+	@Test
+	public void test4() {
+		System.out.println("Demo4");
+	}
+}
